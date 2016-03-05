3aba5146e857b8c6a1283d69f7fae655e2df6881
Kristoffer Walker <kris@kixx.name>
2016-03-05 18:04:56 -0500

    Doing new builds :)
    	deleted:    bin/treadmill
    	new file:   bin/treadmill.js
    	modified:   lib/task.js
    	modified:   lib/task_runner.js
    	new file:   lib/treadmill.js
    	modified:   package.json
    	new file:   treadmill-cli.js

---
+++ treadmill-cli.js
@@ -0,0 +1,36 @@
+'use strict';
+var yargs = require('yargs');
+var filepath = require('filepath');
+var treadmill = require('./lib/treadmill');
+
+exports.main = function () {
+	var options = yargs
+		.usage('Usage: $0 <script_path> [task_name]')
+		.help();
+
+	var argv = options.argv;
+	var script = argv._[0];
+	var task = argv._[1];
+
+	if (!script) {
+		console.log('Missing first <script_path> argument.');
+		options.showHelp();
+		process.exit(1);
+	}
+	if (!task) {
+		console.log('Missing second <task_name> argument.');
+		options.showHelp();
+		process.exit(1);
+	}
+
+	return exports.run(script, task);
+};
+
+exports.run = function (script, task) {
+	require(filepath.create(script).toString());
+	return treadmill.run(task);
+};
+
+if (require.main === module) {
+	exports.main();
+}
